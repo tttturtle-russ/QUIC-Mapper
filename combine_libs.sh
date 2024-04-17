ce1679dd44cd087a908e8dda66d635a2298385db
megaknight-zr <zhuoranch@outlook.com>
2024-04-17 16:22:29 +0800

    delete cpp files

--- combine_libs.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-mkdir -p combined_lib
-cd combined_lib
-for lib in /home/russ/mvfst/_build/lib/libmvfst*.a; do ar -x $lib; done
-ar -q libmvfst.a *.o
-mv libmvfst.a ../
-cd ..
-# rm ./combined_lib -r
