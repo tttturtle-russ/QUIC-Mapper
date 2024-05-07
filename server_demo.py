a778d94bdce0f8b4fe45c5ced7007c89bb5e7b9f
megaknight-zr <zhuoranch@outlook.com>
2024-05-07 17:10:57 +0800

    fix spell

--- server_demo.py
+++ server_demo.py
@@ -4,14 +4,14 @@ from aioquic.quic.configuration import QuicConfiguration
 import contextlib
 
 import os
-SERVER_CACERTFILE = os.path.join(os.getcwd(), "verify", "pycacert.pem")
+SERVER_CACERTFILE = os.path.join(os.getcwd(), "vertify", "pycacert.pem")
 
-SERVER_CERTFILE = os.path.join(os.getcwd(), "verify", "ssl_cert.pem")
+SERVER_CERTFILE = os.path.join(os.getcwd(), "vertify", "ssl_cert.pem")
 SERVER_CERTFILE_WITH_CHAIN = os.path.join(
-    os.getcwd(), "verify", "ssl_cert_with_chain.pem"
+    os.getcwd(), "vertify", "ssl_cert_with_chain.pem"
 )
-SERVER_KEYFILE = os.path.join(os.getcwd(), "verify", "ssl_key.pem")
-SERVER_COMBINEDFILE = os.path.join(os.getcwd(), "verify", "ssl_combined.pem")
+SERVER_KEYFILE = os.path.join(os.getcwd(), "vertify", "ssl_key.pem")
+SERVER_COMBINEDFILE = os.path.join(os.getcwd(), "vertify", "ssl_combined.pem")
 # SKIP_TESTS = frozenset(os.environ.get("AIOQUIC_SKIP_TESTS", "").split(","))
 
 def handle_stream(reader, writer):
